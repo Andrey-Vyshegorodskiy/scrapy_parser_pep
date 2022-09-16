3b3ad4f1f227026136734156a8f13b08f0ae3e50
Andrey-Vyshegorodskiy <93198379+Andrey-Vyshegorodskiy@users.noreply.github.com>
2022-09-16 14:15:30 +0300

    pep 0.3

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,33 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime as dt
 
+from pathlib import Path
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+BASE_DIR = Path(__file__).parent.parent
 
 
 class PepParsePipeline:
+    statuses = {}
+    total = 0
+
+    def open_spider(self, spider):  # наличие этого метода проверяется при тестировании
+        pass
+
     def process_item(self, item, spider):
+        if item.get('status'):
+            self.total += 1
+            self.statuses[item['status']] = (
+                self.statuses.get(item['status'], 0) + 1)
         return item
+
+    def close_spider(self, spider):
+        result_dir = BASE_DIR / 'results'
+        result_dir.mkdir(exist_ok=True)
+        file_path = result_dir / (
+            f'status_summary_'
+            f'{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
+        )
+        with open(file_path, mode='w', encoding='utf-8') as f:
+            f.write('Статус,Количество\n')
+            for status in self.statuses:
+                f.write(f'{status},{self.statuses[status]}\n')
+            f.write(f'Total,{self.total}\n')
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,22 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        for pep_link in response.css('section#numerical-index td a::attr(href)'):
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        title = response.css('h1.page-title::text').get()
+        data = {
+            'number': title.partition(' – ')[0].replace('PEP ', ''),
+            'name': title,
+            'status': response.css('dt:contains("Status") + dd::text').get()
+        }
+        yield PepParseItem(data)
